5654e4b44ac9ca242af793db331a04b726a076f6
Abdull0100 <92032514+Abdull0100@users.noreply.github.com>
2022-02-20 23:27:55 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,26 +1,21 @@
-import socket
+import s ocket
 import select
 import errno
 import sys
 
 HEADER_LENGTH = 10
 
-IP = "127.0.0.1"
+IP = "0.0.0.0"
 PORT = 1234
 my_username = input("Username: ")
 
-# Create a socket
-# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
-# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to a given ip and port
 client_socket.connect((IP, PORT))
 
-# Set connection to non-blocking state, so .recv() call won;t block, just return some exception we'll handle
+# Set connection to non-blocking state, so .recv() call won't block, just return some exception we'll handle
 client_socket.setblocking(False)
 
-client_byte = '0'
 # Prepare username and header and send them
 # We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
 username = my_username.encode('utf-8')
@@ -47,21 +42,7 @@ while True:
     try:
         # Now we want to loop over received messages (there might be more than one) and print them
         while True:
-
-            cb = client_socket.recv(1)
-            client_byte = int(cb.decode('utf-8'))
-            # print(f"cb:{client_byte}")
-
-            if client_byte == 1:
-                # print("Received special data")
-                special_message_header = client_socket.recv(HEADER_LENGTH)
-                special_message_length = int(special_message_header.decode('utf-8').strip())
-                special_message = client_socket.recv(special_message_length)
-                special_message = special_message.decode('utf-8')
-                print(special_message)
-                # cb = 0
-                # continue
-
+                
             # Receive our "header" containing username length, it's size is defined and constant
             username_header = client_socket.recv(HEADER_LENGTH)
 
@@ -86,9 +67,6 @@ while True:
 
     except IOError as e:
         # This is normal on non blocking connections - when there are no incoming data error is going to be raised
-        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
-        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
-        # If we got different error code - something happened
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
             print('Reading error: {}'.format(str(e)))
             sys.exit()
